fd4c07e5fb4cad44d44df3b26413d0bddb1795a1
Roman <kaire1996@gmail.com>
2018-12-14 14:32:24 +0300

    finish

--- prepaire_dataset.sh
+++ prepaire_dataset.sh
@@ -1,3 +1,4 @@
+mkdir dataset
 echo "Downloading dataset"
 wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz
 gzip reviews_Cell_Phones_and_Accessories_5.json.gz
