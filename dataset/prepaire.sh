fa11db444b8e713b74da2fcbee58da2cd6201163
Roman <kaire1996@gmail.com>
2018-12-14 14:20:19 +0300

    inited baseline

---
+++ dataset/prepaire.sh
@@ -0,0 +1,5 @@
+echo "Downloading dataset"
+wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz
+gzip reviews_Cell_Phones_and_Accessories_5.json.gz
+mv reviews_Cell_Phones_and_Accessories_5.json.gz data.json
+echo "Unpack finished, work with data.json file"
