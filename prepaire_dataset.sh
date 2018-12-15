23e4bcd26b5af63fcbf5aa82dcf09eeca6eb26c6
Roman <kaire1996@gmail.com>
2018-12-15 13:15:53 +0300

    updated

--- prepaire_dataset.sh
+++
@@ -1,6 +0,0 @@
-mkdir dataset
-echo "Downloading dataset"
-wget http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz
-gzip reviews_Cell_Phones_and_Accessories_5.json.gz
-mv reviews_Cell_Phones_and_Accessories_5.json.gz dataset/data.json
-echo "Unpack finished, work with data.json file"
